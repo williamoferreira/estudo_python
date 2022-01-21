c0a8d307a0416d9eac3bcee497f6b95e42f92d91
William Ferreira (Age) <william.ferreira@soc.com.br>
2022-01-21 17:02:51 -0300

    Inclusão de comentários para explicar o que os scripts fazem

--- estudo.py
+++ estudo.py
@@ -1,11 +1,24 @@
+# Para escrever no console use o comando print
 print("Hello world");
+
+# if eh parecido com C e C# mas inicio do bloco sera o caractere :
 if (5 > 2): 
     print ("Cinco eh maior que dois")
 
+# Para criar arrays basta colocar o nome do array e uma atribuiacao seguida de colchetes.
+# Preencher os colchetes faz o array vir preenchido com os elementos dos colchetes
+# Use virgulas ',' para separar os elemetos 
 stringNumbers = ["1", "2", "3", "4", "5"]
+
+# Imprimindo todos os elementos do array
 print(stringNumbers)
+
+# Imprimindo o elemento de indice 2 (terceiro elemento, uma vez que o zero conta na lista)
 print(stringNumbers[2])
 
+
 numbers = [ 1, 3, 10, 25, 10, 2]
+# Loop foreach no python
 for number in numbers :
+    # Concatenacao de strings e typecasting uma vez que a variavel number é do tipo int
     print("current number: " + str(number))
--- teste_csv.py
+++ teste_csv.py
@@ -1,7 +1,29 @@
+# Importando a biblioteca para trabalhar com arquivos CSV
 import csv
+
+# Fazendo um link do arquivo com uma variável. Esse link é uma forma de representar o arquivo no código
 arquivo = open('ramais.csv')
+
+# Usando a biblioteca CSV para ler o conteúdo do arquivo usando o link associado anteriormente
 tabela = csv.reader(arquivo)
+
+# Esse trecho é gambiarra. Como a primeira linha do arquivo ramais.csv contém apenas os nomes das colunas.
+# Manipulacao de arquivos tem um conceito importante que chamamos de cursor (ou ponteiro). 
+# Toda linguagem lê uma sequencia de bytes a partir do cursor para manipular.
+# Essa sequencia de bytes pode ser determinada de 
+#   * forma fixa
+#       > muitas vezes determinada pelo tamanho de uma variável de usada de máscara para leitura
+#   * forma variável
+#       > usando algum delimitador dentro do arquivo, como um caratere especial, uma quebra de linha
+# Com o comando next, o python usa a quebra de linha para determinar quantos bytes está lendo.
+# Ou seja, se eu usar várias vezes o comando next estará lendo o arquivo `linha-a-linha`.
+# O comando next retorna a linha lida pelo comando.
 cabecalho = next(tabela)
+
+# Como o comando acima, desloquei o cursor para a segunda linha. Agora uso um loop foreach para percorrer as linhas do csv
 for linha in tabela :
-    print("Nome: " + linha[0] + " - Ramal: " +linha[1])
+    # Como eh um arquivo CSV, a linha retornada eh um array onde cada indice representa uma coluna
+    print("Nome: " + linha[0] + " - Ramal: " + linha[1])
+
+# Apos uso, desfaco o link para liberar o arquivo
 arquivo.close()
